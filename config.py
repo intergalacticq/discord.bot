ceb8b210a9eb78523fd8dd6ab34e561ce6b510fa
intergalacticq <75696471+intergalacticq@users.noreply.github.com>
2022-08-12 23:57:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import discord
+settings = {
+    'token': 'OTUxMDUxMzA5NzY5OTc3ODk4.GkH66A.OX34IAEG8d_rXTrQl4sNWpawc2wJpWgaKsJvHs',
+    'bot': 'Aneko',
+    'id': 951051309769977898,
+    'prefix': '!'
+}
